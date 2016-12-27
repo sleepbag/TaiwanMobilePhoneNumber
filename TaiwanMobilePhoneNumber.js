82de8cb6c13c034183db36df5e82b3a85793d812
x870381 <x870381@gmail.com>
2016-12-28 00:37:49 +0800

    add mix test and  passed.

--- TaiwanMobilePhoneNumber.js
+++ TaiwanMobilePhoneNumber.js
@@ -14,32 +14,34 @@ class TaiwanMobilePhoneNumber {
     for(var i =0; i < stringArray.length; i++){
 
       var tmp_text = stringArray[i].trim();
+      var phone = "";
       //
-      var phone = regEx_First(tmp_text,"^09\\d{2}-?\\d{3}-?\\d{3}");
-      if(phone.length==10){break;}
-      //
-      if (phone == ""){
-        phone = regEx_First(tmp_text,"\\s+09\\d{2}-?\\d{3}-?\\d{3}");
-      }
-      if(phone.length==10){break;}
-      //
-      if(phone == ""){
-        var p = regEx_First(tmp_text,"^8869\\d{2}-?\\d{3}-?\\d{3}");
-        if(p != ""){
-          phone = p.replace("886","0");
+      if(tmp_text.length==10){
+        phone = regEx_First(tmp_text,"^09\\d{2}-?\\d{3}-?\\d{3}");
+        if (phone == ""){
+          phone = regEx_First(tmp_text,"\\s+09\\d{2}-?\\d{3}-?\\d{3}");
         }
       }
-      if(phone.length==10){break;}
-      //
-      if(phone == ""){
-        var p = regEx_First(tmp_text,"\\+8869\\d{2}-?\\d{3}-?\\d{3}");
-        if(p != ""){
-          phone = p.replace("\+886","0");
-        }
-      }
-      if(phone.length==10){break;}
-      //
 
+      if(tmp_text.length==12){
+        //
+        if(phone == ""){
+          var p = regEx_First(tmp_text,"^8869\\d{2}-?\\d{3}-?\\d{3}");
+          if(p != ""){
+            phone = p.replace("886","0");
+          }
+        }
+      }
+
+      if(tmp_text.length==13){
+        //
+        if(phone == ""){
+          var p = regEx_First(tmp_text,"\\+8869\\d{2}-?\\d{3}-?\\d{3}");
+          if(p != ""){
+            phone = p.replace("\+886","0");
+          }
+        }
+      }
     }
     return phone;
   }
--- tests.js
+++ tests.js
@@ -1,22 +1,22 @@
 QUnit.test( "origin test", function( assert ) {
 
-  var mobile = new TaiwanMobilePhoneNumber("0913544966");
+  var mobile = new TaiwanMobilePhoneNumber("0932132123");
   var result = mobile.Process();
-  assert.ok( result == "0913544966", "Passed!" );
+  assert.ok( result == "0932132123", "Passed!" );
 });
 
 QUnit.test( "international code test", function( assert ) {
 
-  var mobile = new TaiwanMobilePhoneNumber("886913544966");
+  var mobile = new TaiwanMobilePhoneNumber("886932132123");
   var result = mobile.Process();
-  assert.ok( result == "0913544966", "Passed!" );
+  assert.ok( result == "0932132123", "Passed!" );
 });
 
 QUnit.test( "international plus code test", function( assert ) {
 
-  var mobile = new TaiwanMobilePhoneNumber("+886913544966");
+  var mobile = new TaiwanMobilePhoneNumber("+886932132123");
   var result = mobile.Process();
-  assert.ok( result == "0913544966", "Passed!" );
+  assert.ok( result == "0932132123", "Passed!" );
 });
 
 QUnit.test( "preAlpha code test", function( assert ) {
@@ -35,8 +35,39 @@ QUnit.test( "preAlpha code 2 test", function( assert ) {
 
 QUnit.test( "mix test", function( assert ) {
 
-  var mobile = new TaiwanMobilePhoneNumber("D09575959419 09575959419 0932050119");
+  var mobile = new TaiwanMobilePhoneNumber("D09575959419 Y0937383100 09575959419 0932132123");
+  var result = mobile.Process();
+  assert.ok( result == "0932132123", "Passed!" );
+});
+
+QUnit.test( "mix test 2", function( assert ) {
+
+  var mobile = new TaiwanMobilePhoneNumber("D09575959419 Y0937383100 09575959419 886932132123");
   var result = mobile.Process();
   console.log(result);
-  assert.ok( result == "0932050119", "Passed!" );
+  assert.ok( result == "0932132123", "Passed!" );
+});
+
+QUnit.test( "mix test 3", function( assert ) {
+
+  var mobile = new TaiwanMobilePhoneNumber("D09575959419 Y0937383100 09575959419 +886932132123");
+  var result = mobile.Process();
+  console.log(result);
+  assert.ok( result == "0932132123", "Passed!" );
+});
+
+QUnit.test( "mix test 4", function( assert ) {
+
+  var mobile = new TaiwanMobilePhoneNumber("D09575959419 Y886932132124 09575959419 +886932132123");
+  var result = mobile.Process();
+  console.log(result);
+  assert.ok( result == "0932132123", "Passed!" );
+});
+
+QUnit.test( "mix test 5", function( assert ) {
+
+  var mobile = new TaiwanMobilePhoneNumber("D09575959419 Y886932132124 09575959419 886932132123");
+  var result = mobile.Process();
+  console.log(result);
+  assert.ok( result == "0932132123", "Passed!" );
 });
