d7dfe41dbe974ba161f9d23cd27c047152cb2875
x870381 <x870381@gmail.com>
2017-06-09 20:47:46 +0800

    新增 replaceIndex 功能

--- regExLib.js
+++ regExLib.js
@@ -9,3 +9,34 @@ function regEx_First(text,reg){
     return ex[0];
   }else{return"";}
 }
+
+function getStrinSegmentTail(string,segment){
+
+  var tmpstring = "";
+  for(var i = 0;i<string.length;i++){
+    tmpstring+=string[i];
+    var reg = ".+" + segment;
+    var result = regEx_First(tmpstring,reg);
+    if (result!="")
+    {
+      return result;
+    }
+  }
+  return false;
+}
+
+function isCnNumber(string){
+  var cnNumberchar = "[零一二三四五六七八九十百千萬億兆亿万]";
+  var ex = string.match(cnNumberchar);
+  if (checkArray(ex)){
+    return ex[0];
+  }else{return false;}
+}
+
+function replaceIndex(string, at, search, repl) {
+  return string.replace(
+    RegExp("^(?:.*?"+search+"){" + at + "}"),
+     function(x){return x.replace(RegExp(search + "$"),
+      repl)})
+
+}
